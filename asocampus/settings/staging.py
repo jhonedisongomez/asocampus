3cebdc5a619f92695ed271a4ab6e6d4dfa013772
Jhon  Edison Gomez <jhon_edinson11@hotmail.com>
2017-10-04 13:19:02 -0500

    adjust files to deploy in heroku

---
+++ asocampus/settings/staging.py
@@ -0,0 +1,19 @@
+
+from .base import *
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+TEMPLATE_DEBUG = True
+ALLOWED_HOSTS = ['*']
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'asocampus',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': '192.168.1.59',
+        'PORT': '5432',
+    }
+}
+
